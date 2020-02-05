fd0b8ba00e4ea313da3086e4ecc1d8fec166ff5e
Sonia Wang <sonia@sonias-mbp.risd.edu>
2020-02-05 13:46:26 -0500

    first commit

---
+++ main copy.js
@@ -0,0 +1,117 @@
+function box41(){
+    var box1 = document.querySelector(".box1");
+    var box2 = document.querySelector(".box2");
+    var box3 = document.querySelector(".box3");
+    var box4 = document.querySelector(".box4");
+    var whatisbox = document.querySelector(".whatisbox")
+    var text = document.querySelector(".codetext");
+    box1.style.marginTop = "175px";
+    box2.style.marginTop = "175px";
+    box3.style.marginTop = "175px";
+    whatisbox.style.visibility = "visible";
+    text.style.visibility = "visible";
+    // box4.style.transform= "scale(1.5)";
+    console.log("testing");
+}
+
+function box42(){
+    var box1 = document.querySelector(".box1");
+    var box2 = document.querySelector(".box2");
+    var box3 = document.querySelector(".box3");
+    var box4 = document.querySelector(".box4");
+    var whatisbox = document.querySelector(".whatisbox")
+    var text = document.querySelector(".codetext");
+    box1.style.marginTop = "";
+    box2.style.marginTop = "";
+    box3.style.marginTop = "";
+    box4.style.transform= "";
+    whatisbox.style.visibility = "";
+    text.style.visibility = "";
+    console.log("testing");
+}
+
+function box31(){
+    var box1 = document.querySelector(".box1");
+    var box2 = document.querySelector(".box2");
+    var box4 = document.querySelector(".box4");
+    var coffee = document.querySelector(".coffee");
+    var teacup = document.querySelector(".teacup");
+    box1.style.marginTop = "175px";
+    box2.style.marginTop = "175px";
+    box4.style.marginTop = "-50px";
+    coffee.style.visibility = "visible";
+    teacup.style.visibility = "visible";
+    console.log("testing");
+}
+
+function box32(){
+    var box1 = document.querySelector(".box1");
+    var box2 = document.querySelector(".box2");
+    var box4 = document.querySelector(".box4");
+    var coffee = document.querySelector(".coffee");
+    var teacup = document.querySelector(".teacup");
+    box1.style.marginTop = "";
+    box2.style.marginTop = "";
+    box4.style.marginTop = "";
+    coffee.style.visibility = "";
+    teacup.style.visibility = "";
+    console.log("testing");
+}
+
+
+function box21(){
+    var box1 = document.querySelector(".box1");
+    var box3 = document.querySelector(".box3");
+    var box4 = document.querySelector(".box4");
+    var mount = document.querySelector(".mount");
+    var mountains = document.querySelector(".mountains");
+    box1.style.marginTop = "175px";
+    box3.style.marginTop = "-50px";
+    box4.style.marginTop = "-50px";
+    mount.style.visibility = "visible";
+    mountains.style.visibility = "visible";
+    console.log("testing");
+}
+
+function box22(){
+    var box1 = document.querySelector(".box1");
+    var box3 = document.querySelector(".box3");
+    var box4 = document.querySelector(".box4");
+    var mount = document.querySelector(".mount");
+    var mountains = document.querySelector(".mountains");
+    box1.style.marginTop = "";
+    box3.style.marginTop = "";
+    box4.style.marginTop = "";
+    mount.style.visibility = "";
+    mountains.style.visibility = "";
+    console.log("testing");
+}
+
+
+function box11(){
+    var box2 = document.querySelector(".box2");
+    var box3 = document.querySelector(".box3");
+    var box4 = document.querySelector(".box4");
+    var boob = document.querySelector(".boob");
+    var responsee = document.querySelector(".responsee");
+    box2.style.marginTop = "-50px";
+    box3.style.marginTop = "-50px";
+    box4.style.marginTop = "-50px";
+    boob.style.visibility = "visible";
+    responsee.style.visibility = "visible";
+    console.log("testing");
+}
+
+function box12(){
+    var box2 = document.querySelector(".box2");
+    var box3 = document.querySelector(".box3");
+    var box4 = document.querySelector(".box4");
+    var boob = document.querySelector(".boob");
+    var responsee = document.querySelector(".responsee");
+    box2.style.marginTop = "";
+    box3.style.marginTop = "";
+    box4.style.marginTop = "";
+    boob.style.visibility = "";
+    responsee.style.visibility = "";
+    console.log("testing");
+}
